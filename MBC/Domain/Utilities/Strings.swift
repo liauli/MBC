56f1d28afea1da32dc0790562519484773db015b
Aulia Nastiti <aulia_nastiti@telkomsel.co.id>
2026-05-22 20:57:10 +0700

    feat: [#49] Add localization (id/en) + getString helper + error userMessage

---
+++ MBC/Domain/Utilities/Strings.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func getString(_ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, comment: "")
+    return args.isEmpty ? format : String(format: format, arguments: args)
+}
