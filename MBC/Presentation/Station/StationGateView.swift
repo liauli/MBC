ed0ddbf00e2fc4dc53f3c49c3cd6db3cc046f09d
Aulia Nastiti <aulia_nastiti@telkomsel.co.id>
2026-05-22 19:50:32 +0700

    feat: [#39][#40] PIN UI (Entry, Setup) + StationGate flow

--- MBC/Presentation/Station/StationGateView.swift
+++ MBC/Presentation/Station/StationGateView.swift
@@ -2,9 +2,19 @@ import MBCDesignSystem
 import SwiftUI
 
 struct StationGateView: View {
+    @StateObject private var pinViewModel = ViewModelProvider.instance.providePINViewModel()
+
     var body: some View {
-        StationHomeView()
-            .navigationTitle("Station")
-            .navigationBarTitleDisplayMode(.inline)
+        Group {
+            if pinViewModel.state == .authenticated {
+                StationHomeView()
+            } else if pinViewModel.needsSetup {
+                PINSetupView(viewModel: pinViewModel)
+            } else {
+                PINEntryView(viewModel: pinViewModel)
+            }
+        }
+        .navigationTitle("Station")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
