fb0d361093e302b8f6bc14c7f41a0506e4e6e011
Aulia Nastiti <aulia_nastiti@telkomsel.co.id>
2026-05-22 20:57:10 +0700

    feat: [#49] Add localization (id/en) + getString helper + error userMessage

--- MBC/Domain/Models/MBCError.swift
+++ MBC/Domain/Models/MBCError.swift
@@ -14,4 +14,35 @@ enum MBCError: Error, Equatable {
     case deserializationFailed
     case encryptionFailed
     case decryptionFailed
+
+    var userMessage: String {
+        switch self {
+        case .nfcNotAvailable:
+            getString("error.nfc.unavailable")
+        case .nfcReadFailed:
+            getString("error.nfc.read")
+        case .nfcWriteFailed:
+            getString("error.nfc.write")
+        case .cardNotRegistered:
+            getString("error.card.unregistered")
+        case .alreadyCheckedIn:
+            getString("error.already.checkedin")
+        case .notCheckedIn:
+            getString("error.not.checkedin")
+        case let .insufficientBalance(required, available):
+            getString("error.insufficient.balance", required.currencyFormatted, available.currencyFormatted)
+        case .invalidName:
+            getString("error.invalid.name")
+        case .invalidAmount:
+            getString("error.invalid.amount")
+        case .serializationFailed:
+            getString("error.serialization")
+        case .deserializationFailed:
+            getString("error.deserialization")
+        case .encryptionFailed:
+            getString("error.encryption")
+        case .decryptionFailed:
+            getString("error.decryption")
+        }
+    }
 }
